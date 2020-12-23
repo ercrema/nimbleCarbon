7d48ac27f2a1391d18ddb24ec91e9c955883699d
Enrico R. Crema <enrico.crema@gmail.com>
2020-12-23 20:53:32 +0000

    Posterior predictive check functions

---
+++ R/plot.spdppc.R
@@ -0,0 +1,162 @@
+#' @title Plot SPD-based Posterior Predictive Check
+#'
+#' @description Plots spdppc' class object for SPD-based Posterior Predictive Check.
+#' @param x An 'spdppc' class object.
+#' @param type Either a 'spaghetti' plot or a quantile based envelope plot. Default is 'envelope'.
+#' @param interval Quantile interval used for the envelope plot. Ignored when \code{type} is set to 'spaghetti'. Default is 0.90.
+#' @param nsample Number of samples to be displayed in the 'spaghetti' plot. Default is the total number of simulations supplied in the 'spdppc' class object, ignored when \code{type} is set to 'envelope'.
+#' @param obs.lwd Line width of the observed SPD. Default is 1.5.
+#' @param obs.col Line colour of the observed SPD. Default is 'black'.
+#' @param sim.col Line colour of simulated SPDs. Default is 'lightgrey', ignored when \code{type} is set to 'envelope'.
+#' @param alpha Transparency value for each line in the spaghetti plot. Default is 1, ignored when \code{type} is set to 'envelope'.
+#' @param envelope.col Fill colour of the simulation envelope. Default is 'lightgrey', ignored when \code{type} is set to 'envelope.'spaghetti'.
+#' @param positive.col Fill colour for the area with positive deviation from the simulation envelope.  Default is 'red', ignored when \code{type} is set to 'spaghetti'.
+#' @param negative.col Fill colour for the area with positive deviation from the simulation envelope.  Default is 'blue', ignored when \code{type} is set to 'spaghetti'.
+#' @param calendar  Either \code{'BP'} or \code{'BCAD'}. Indicate whether the calibrated date should be displayed in BP or BC/AD. Default is  \code{'BP'}.
+#' @param ... Additional arguments affecting the plot
+#' @method plot spdppc
+#' @export  
+
+plot.spdppc = function(x, type='envelope', nsample=NULL, interval=0.90, obs.lwd=1.5,obs.col='black',sim.col='lightgrey',alpha=1,envelope.col='lightgrey',positive.col='red',negative.col='blue',calendar='BP', ...)
+{
+  if (!type%in%c('spaghetti','envelope')) {stop("The argument 'type' should be either 'spaghetti' or 'envelope'.")}
+  if (is.null(nsample)) {nsample = ncol(x$simmatrix)}
+  if (type=='spaghetti' & nsample > ncol(x$simmatrix))
+  {
+    warning(paste0('nsample large than the number of posterior simulations. Running with nsample=',ncol(x$simmatrix)))
+    nsample = ncol(x$simmatrix)
+  }
+  
+  #Setting calendar
+  if (calendar=="BP"){
+    plotyears <- x$obs$calBP
+    xlabel <- "Years cal BP"
+    xlim <- c(max(plotyears),min(plotyears))
+  } else if (calendar=="BCAD"){
+    plotyears <- BPtoBCAD(a:b)
+    xlabel <- "Years BC/AD"
+    if (all(range(plotyears)<0)){xlabel <- "Years BC"}
+    if (all(range(plotyears)>0)){xlabel <- "Years AD"}
+    xlim <- c(min(plotyears),max(plotyears))
+  } else {
+    stop("Unknown calendar type")
+  }
+  
+  
+  
+  if (type=='envelope')
+  {
+    lo = apply(x$simmatrix,1,quantile,prob=(1-interval)/2)
+    hi = apply(x$simmatrix,1,quantile,prob=1-(1-interval)/2)
+    obs = x$obs$PrDens
+    ylim = c(0,max(hi,x$obs$PrDens))
+    
+    # Boom and Bust Handling ####
+    booms <- which(obs>hi)
+    busts <- which(obs<lo)
+    baseline <- rep(NA,length(obs))
+    colpts = rep('grey',length(obs))
+    colpts[booms] = 'red'
+    colpts[busts] = 'blue'
+    
+    boomPlot <- baseline
+    if (length(booms)>0){ boomPlot[booms]=obs[booms] }
+    bustPlot <- baseline
+    if (length(busts)>0){ bustPlot[busts]=obs[busts] }           
+    
+    boomBlocks <- vector("list")
+    counter <- 0
+    state <- "off"
+    for (i in 1:length(boomPlot)){
+      if (!is.na(boomPlot[i])&state=="off"){
+        counter <- counter+1
+        boomBlocks <- c(boomBlocks,vector("list",1))
+        boomBlocks[[counter]] <- vector("list",2)
+        boomBlocks[[counter]][[1]] <- boomPlot[i]
+        boomBlocks[[counter]][[2]] <- plotyears[i]
+        state <- "on"
+      }
+      if (state=="on"){
+        if (!is.na(boomPlot[i])){
+          boomBlocks[[counter]][[1]] <- c(boomBlocks[[counter]][[1]],boomPlot[i])
+          boomBlocks[[counter]][[2]] <- c(boomBlocks[[counter]][[2]],plotyears[i])
+        }
+        if (is.na(boomPlot[i])){
+          state <- "off"
+        }
+      }   
+    }
+    
+    bustBlocks <- vector("list")
+    counter <- 0
+    state <- "off"
+    for (i in 1:length(bustPlot)){
+      if (!is.na(bustPlot[i])&state=="off"){
+        counter <- counter+1
+        bustBlocks <- c(bustBlocks,vector("list",1))
+        bustBlocks[[counter]] <- vector("list",2)
+        bustBlocks[[counter]][[1]] <- bustPlot[i]
+        bustBlocks[[counter]][[2]] <- plotyears[i]
+        state <- "on"
+      }
+      if (state=="on"){
+        if (!is.na(bustPlot[i])){
+          bustBlocks[[counter]][[1]] <- c(bustBlocks[[counter]][[1]],bustPlot[i])
+          bustBlocks[[counter]][[2]] <- c(bustBlocks[[counter]][[2]],plotyears[i])
+        }
+        if (is.na(bustPlot[i])){
+          state <- "off"
+        }
+      }   
+    }
+    
+    plot(0, 0, xlim=xlim, ylim=ylim, type="n", col="white", ylab='Probability', xlab=xlabel, xaxt="n", ...)
+    
+    polygon(c(plotyears,rev(plotyears)),c(lo,rev(hi)),col=envelope.col,border=NA)
+    
+    if (length(booms)>0){
+      for (i in 1:length(boomBlocks)){
+        bbb = unique(boomBlocks[[i]][[2]])
+        index = which(plotyears%in%bbb)
+        polygon(c(bbb,rev(bbb)),c(x$obs$PrDens[index],rev(hi[index])),border=NA,col=positive.col)
+      }  
+    }
+    
+    if (length(busts)>0){
+      for (i in 1:length(bustBlocks)){
+        bbb = unique(bustBlocks[[i]][[2]])
+        index = which(plotyears%in%bbb)
+        polygon(c(bbb,rev(bbb)),c(x$obs$PrDens[index],rev(lo[index])),border=NA,col=negative.col)
+      }  
+    }
+    lines(plotyears,x$obs$PrDens,lwd=1)
+  } 
+  
+  if (type=='spaghetti')
+  {
+    simmat = x$simmatrix[,sample(1:ncol(x$simmatrix),size=nsample)]
+    ylim=c(0,max(x$obs$PrDens,simmat))
+    plot(0, 0, xlim=xlim, ylim=ylim, type="n", col="white", ylab='Probability', xlab=xlabel, xaxt="n", ...)
+    sim.col=col2rgb(sim.col)
+    sim.col = rgb(sim.col[1,]/255,sim.col[2,]/255,sim.col[3,]/255,alpha=alpha)
+    apply(simmat,2,lines,x=plotyears,col=sim.col)
+    lines(plotyears,x$obs$PrDens,lwd=obs.lwd,col=obs.col)
+  }
+
+  if (calendar=="BP"){
+    rr <- range(pretty(plotyears))    
+    axis(side=1,at=seq(rr[2],rr[1],-100),labels=NA,tck = -.01)
+    axis(side=1,at=pretty(plotyears),labels=abs(pretty(plotyears)))
+  } else if (calendar=="BCAD"){
+    yy <-  plotyears
+    rr <- range(pretty(yy))    
+    prettyTicks <- seq(rr[1],rr[2],+100)
+    prettyTicks[which(prettyTicks>=0)] <-  prettyTicks[which(prettyTicks>=0)]-1
+    axis(side=1,at=prettyTicks, labels=NA,tck = -.01)
+    py <- pretty(yy)
+    pyShown <- py
+    if (any(pyShown==0)){pyShown[which(pyShown==0)]=1}
+    py[which(py>1)] <-  py[which(py>1)]-1
+    axis(side=1,at=py,labels=abs(pyShown))
+  }
+}
---
+++ R/postPredSPD.R
@@ -0,0 +1,125 @@
+#' @title SPD-based Posterior Predictive Check
+#'
+#' @description Generates SPDs from posterior samples.
+#' @param x A vector of observed uncalibrated radiocarbon ages.
+#' @param errors A vector of standard deviations corresponding to each estimated radiocarbon age.
+#' @param calCurve Character string naming a calibration curve already provided with the rcarbon package (currently 'intcal20','intcal13','intcal13nhpine16','shcal20','shcal13','shcal13shkauri16',''marine13','marine20').
+#' @param model Growth Model
+#' @param a lower (earliest) limit of the distribution (in BP).
+#' @param b upper (latest) limit of the distribution (in BP).
+#' @param params List of vectors containing model parameters. The names attribute of each vector should match growth model parameters.
+#' @param nsim Number of SPDs to be generated.  Default is the length of the parameter vectors supplied in the argument \code{params}.
+#' @param method Method for the creation of random dates from the fitted model. Either 'uncalsample' or 'calsample'. Default is 'calsample'.
+#' @param spdnormalised A logical variable indicating whether the total probability mass of the SPD is normalised to sum to unity for both observed and simulated data. Default is TRUE.
+#' @param ncores Number of cores used for for parallel execution. Default is 1.
+#' @param verbose A logical variable indicating whether extra information on progress should be reported. Default is TRUE.
+#' @return An object of class \code{spdppc} with the following elements
+#' \itemize{
+#' \item{\code{obs}} {A data.frame containing the years (in Cal BP) and the corresponding summed probability in the observed data.}
+#' \item{\code{spdmat}} {A matrix containing the summed probability distribution of the simulated data.}
+#' }
+#' @import stats
+#' @import grDevices
+#' @import graphics
+#' @import doSNOW 
+#' @import snow
+#' @import foreach 
+#' @import rcarbon
+#' @export 
+
+postPredSPD = function(x,errors,calCurve,model,a,b,params,nsim,method='uncalsample',spdnormalised=TRUE,ncores=1,verbose=TRUE)
+{
+  #Sanity checks ####
+  if (length(unique(unlist(lapply(params,length))))>1)
+  {
+    print('Number of supplied sample parameters should be the same.')
+    stop()
+  }
+  
+  if (unique(unlist(lapply(params,length)))<nsim)
+  {
+    print(paste0('The argument nsim is larger than the supplied length of parameter values. Running with nsim=',unique(unlist(lapply(params,length)))))
+    stop()
+  }
+  
+  if (ncores>1&!requireNamespace("doSNOW", quietly=TRUE)){	
+    warning("the doSNOW package is required for multi-core processing; ncores has been set to 1")
+    ncores=1
+  }	
+  
+  if (!any(method%in%c("uncalsample","calsample")))
+  {
+    stop("The 'method' argument must be either 'uncalsample' or 'calsample'")
+  }
+  
+  
+  # General Settings ####
+  #randomly sample nsim parameter values
+  params = lapply(params,sample,size=nsim)
+  ndates = length(x)
+  calBP = a:b
+  
+  #Calibrate Observed Data and Generate SPD ####
+  if(verbose){'Generate observed SPD'}
+  calibrated.dates=calibrate(x,errors,calCurves = calCurve,verbose=verbose)
+  obs.spd = spd(calibrated.dates,timeRange=c(a,b),spdnormalised = spdnormalised,verbose=verbose)
+
+  
+  
+  
+  
+  #Iterate through posterior ####
+  
+  # Setup foreach loop:
+  opts = NULL
+  if (verbose)
+  {
+    if (ncores>1){ print(paste("Running in parallel on ",getDoParWorkers()," workers...",sep=""))}
+    pb <- txtProgressBar(min=0, max=nsim, style=3)
+    progress <- function(n) setTxtProgressBar(pb, n)
+    opts <- list(progress = progress)
+  }
+  
+  `%dofun%` <- `%do%`
+  if (ncores>1){
+    cl <- snow::makeCluster(ncores)
+    registerDoSNOW(cl)
+    `%dofun%` <- `%dopar%`
+  }
+  
+  # Loop starts here
+  predMat <- foreach (i=1:nsim,.packages = 'rcarbon',.options.snow = opts,.combine='cbind') %dofun% {
+    if (verbose & ncores==1) {setTxtProgressBar(pb, i)}
+    
+    #Create Probability Model
+    PrDens=do.call(model,args=c(list(x=a:b,a=a,b=b),lapply(params,function(x,i){x[[i]]},i=i),list(log=FALSE)))
+  
+    #Uncalibrate
+    uncalGrid=uncalibrate(as.CalGrid(data.frame(calBP=calBP,PrDens=PrDens)),verbose=FALSE)
+    
+    #Sample Dates
+    if (method=='uncalsample')
+    {
+      x.tmp=sample(uncalGrid$CRA,replace=TRUE,size=ndates,prob=uncalGrid$PrDens)
+    }
+    if (method=='calsample')
+    {
+      x.tmp=sample(uncalGrid$CRA,replace=TRUE,size=ndates,prob=uncalGrid$Raw)
+    }
+    
+    #Calibrate
+    x.tmp.calibrated = calibrate(x.tmp,errors = sample(errors,size=ndates,replace=TRUE),calCurves = calCurve, verbose=FALSE)
+    
+    #Make SPD
+    x.tmp.spd = spd(x.tmp.calibrated,timeRange = c(a,b),spdnormalised = spdnormalised,verbose=FALSE)
+    return(x.tmp.spd$grid$PrDens)
+  }
+  if (ncores>1){
+    snow::stopCluster(cl)
+  }
+  if (verbose){ close(pb) }
+  
+  out = list(obs=obs.spd$grid,simmatrix=predMat)
+  class(out) <- c("spdppc",class(out))
+  return(out)
+}
