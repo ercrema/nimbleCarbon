ab08ea1f353cfe18cebdd5fdf0349200d09bd1a0
Enrico R. Crema <enrico.crema@gmail.com>
2021-01-04 09:36:39 +0000

    Remove previous example

--- R/dDoubleExponentialGrowth.R
+++ R/dDoubleExponentialGrowth.R
@@ -21,24 +21,6 @@ NULL
 #' @export
 
 
-# a=7000
-# b=3000
-# mu=6000
-# nt0 = 1
-# nt1 =666666
-# t =  1:(abs(b-a)+1)
-# t1 = 1:(abs(mu-a))
-# t2 = 1:(abs(b-mu)+1)
-# r1=0.004
-# r2=-0.004
-# n = c(nt0*(1+r1)^c(t1),(nt0*(1+r1)^abs(mu-a))*(1+r2)^t2)
-# n1 = c(nt1*(1+r1)^c(t1),(nt1*(1+r1)^abs(mu-a))*(1+r2)^t2)
-# plot(a:b,n,type='l',xlim=c(a,b))
-# abline(v=mu,lty=2)
-# all.equal(n/sum(n),n1/sum(n1))
-
-#dDoubleExponentialGrowth(2000,a=3400,b=1850,r1=0.01,r2=0.003,mu=2000,log=TRUE)
-
 dDoubleExponentialGrowth=nimbleFunction(
   run = function(x = integer(0),a=double(0),b=double(0),r1=double(0),r2=double(0),mu=double(0), log = integer(0)) {
     returnType(double(0))
