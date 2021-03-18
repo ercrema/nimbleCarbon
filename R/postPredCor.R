b22fc03b4c7f341380131c3eed9136fc85574849
Enrico R. Crema <enrico.crema@gmail.com>
2021-03-18 16:51:56 +0000

    Added heuristic correlation function to evaluate model goodness-of-fit

---
+++ R/postPredCor.R
@@ -0,0 +1,17 @@
+#' @title Calculates correlation between observed and posterior generated SPD.
+#' @description  Computes the correlation between observed SPDs and posterior generated SPD from the output of \code{postPredSPD()} function as an heuristic of the goodness-of-fit of the model. 
+#' @param x An object of class \code{spdppc}.
+#' @param method a character string indicating which correlation coefficient is to be computed. One of "pearson" (default), "kendall", or "spearman": can be abbreviated.
+#' @returns A vector of correlation values. 
+#' @export 
+
+postPredCor= function(x,method='pearson')
+{
+  if (!c('spdppc')%in%class(x))
+  {
+    stop('x must be of class "spdppc"')
+  }
+  xx = cbind(x$obs$PrDens,x$simmatrix)
+  res = as.numeric(cor(xx,method=method)[1,])
+  return(res)
+}
